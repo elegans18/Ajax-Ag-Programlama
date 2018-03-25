73a991fcf1f7cd4d8cd2ae4c4c97c39ca2943986
Hüseyin Kahraman <huseyinkahraman18@gmail.com>
2018-03-25 23:19:06 +0300

    düzenleme yapıldı

--- deneme.php
+++ deneme.php
@@ -7,10 +7,36 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <title>Ağ Programlama Ödevi</title>
+
+      <script>
+      $(document).ready(function()
+      {
+        $("#menu").hide();
+        $('.tikla').click(function()
+       {
+          $("#menu").show();//.show()
+       });
+
+      });
+
+      </script>
+
+
   </head>
   <body>
+
+
+
+  <div class="col-sm-12">
+  <table class="table">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">Ders Programı</th>
+    </tr>
+  </thead>
+  </div>
 
     <div class="col-sm-12">
     <table class="table">
@@ -23,51 +49,69 @@
   </thead>
   <tbody>
     <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
+      <th scope="row">0</th>
+      <td>BSM 103</td>
+      <td>Programlamaya Giriş</td>
     </tr>
     <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
+      <th scope="row">1</th>
+      <td>BSM 201</td>
+      <td>Elektrik Devre Temelleri</td>
+    </tr>
+    <tr>
+      <th class="tikla" scope="row" id = "tikla">2</th>
+      <td class="tikla">BSM 205</td>
+      <td class="tikla">Web Programlama</td>
     </tr>
     <tr>
       <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
+      <td>BSM 309</td>
+      <td>İşletim Sistemleri</td>
+    </tr>
+    <tr>
+      <th scope="row">4</th>
+      <td>BSM 458</td>
+      <td>Ağ Programlama</td>
     </tr>
   </tbody>
-</table>
-
-<table class="table">
-  <thead class="thead-light">
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-    </tr>
-  </tbody>
-</table>
+  </table>
     </div>
 
+    <div id = "menu">
+      <div class="input-group mb-3">
+      <div class="input-group-prepend">
+      <span class="input-group-text" id="basic-addon1">Dersin Kodu :</span>
+      </div>
+      <input type="text" class="form-control" aria-label="Username" aria-describedby="basic-addon1">
+      </div>
+
+      <div class="input-group mb-3">
+      <div class="input-group-prepend">
+      <span class="input-group-text" id="basic-addon1">Dersin Adı :</span>
+      </div>
+      <input type="text" class="form-control" aria-describedby="basic-addon1">
+      </div>
+
+      <div class="input-group mb-3">
+      <div class="input-group-prepend">
+      <span class="input-group-text" id="basic-addon1">Dersin İçeriği :</span>
+      </div>
+      <input type="text" class="form-control" aria-describedby="basic-addon1">
+      </div>
+      
+      <div>
+      <button type="button" class="btn btn-primary">XML Göster</button>
+      <button type="button" class="btn btn-primary">JSON Göster</button>
+      </div>
+
+      <div class="input-group">
+      <div class="input-group-prepend">
+      <span class="input-group-text">Kodlanmış Veri</span>
+      </div>
+      <textarea class="form-control" aria-label="With textarea"></textarea>
+    </div>
+
+    </div>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
